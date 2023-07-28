4665a846db64f79246d5dc62720e501b7210e9ef
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-28 18:56:45 +0100

    ♻️ Extracted explaining variable and Methods

--- Kata.Tests/FizzBuzzShould.cs
+++ Kata.Tests/FizzBuzzShould.cs
@@ -14,13 +14,16 @@ namespace Kata.Tests;
  */
 public class FizzBuzzShould
 {
+    private int MAX = 100;
+    private int MIN = 0;
+    
     [Fact]
     public void turn_into_printable_not_special_numbers()
     {
         var generatorOfNotSpecialNumbers = Gen
-            .Choose(0, 100)
-            .Where(number => number % 3 != 0)
-            .Where(number => number % 5 != 0)
+            .Choose(MIN, MAX)
+            .Where(number => !IsDivisibleByThree(number))
+            .Where(number => !IsDivisibleByFive(number))
             .ToArbitrary();
 
         Prop.ForAll<int>(
@@ -34,9 +37,9 @@ public class FizzBuzzShould
     public void turn_into_number_divisible_by_three_to_Fizz_word()
     {
         var generatorOfNotSpecialNumbers = Gen
-            .Choose(0, 100)
-            .Where(number => number % 3 == 0)
-            .Where(number => number % 5 != 0)
+            .Choose(MIN, MAX)
+            .Where(number => IsDivisibleByThree(number))
+            .Where(number => !IsDivisibleByFive(number))
             .ToArbitrary();
         
         Prop.ForAll<int>(
@@ -50,9 +53,9 @@ public class FizzBuzzShould
     public void turn_into_number_divisible_by_five_to_Buzz_word()
     {
         var generatorOfNotSpecialNumbers = Gen
-            .Choose(0, 100)
-            .Where(number => number % 3 != 0)
-            .Where(number => number % 5 == 0)
+            .Choose(MIN, MAX)
+            .Where(number => !IsDivisibleByThree(number))
+            .Where(number => IsDivisibleByFive(number))
             .ToArbitrary();
         
         Prop.ForAll<int>(
@@ -65,7 +68,25 @@ public class FizzBuzzShould
     [Fact]
     public void turn_into_number_divisible_by_five_and_three_to_FizzBuzz_word()
     {
-        int NUMBER_DIVISIBLE_BY_FIVE = 15;
-        new FizzBuzz().execute(NUMBER_DIVISIBLE_BY_FIVE).Should().Be("FizzBuzz");
+        var generatorOfNotSpecialNumbers = Gen
+            .Choose(MIN, MAX)
+            .Where(number => IsDivisibleByThree(number))
+            .Where(number => IsDivisibleByFive(number))
+            .ToArbitrary();
+        
+        Prop.ForAll<int>(
+            generatorOfNotSpecialNumbers, (int number) =>
+            {
+                new FizzBuzz().execute(number).Should().Be("FizzBuzz");
+            }).QuickCheckThrowOnFailure();
+    }
+    
+    private bool IsDivisibleByThree(int number)
+    {
+        return number % 3 == 0;
+    }
+    private bool IsDivisibleByFive(int number)
+    {
+        return number % 5 == 0;
     }
 }
