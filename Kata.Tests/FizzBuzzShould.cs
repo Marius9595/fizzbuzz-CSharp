03ee1c7f31f2112563158156788a5d8b09f8375e
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-28 18:48:37 +0100

    ✅ Cover all not numbers divisible by Five

--- Kata.Tests/FizzBuzzShould.cs
+++ Kata.Tests/FizzBuzzShould.cs
@@ -49,8 +49,17 @@ public class FizzBuzzShould
     [Fact]
     public void turn_into_number_divisible_by_five_to_Buzz_word()
     {
-        int NUMBER_DIVISIBLE_BY_FIVE = 5;
-        new FizzBuzz().execute(NUMBER_DIVISIBLE_BY_FIVE).Should().Be("Buzz");
+        var generatorOfNotSpecialNumbers = Gen
+            .Choose(0, 100)
+            .Where(number => number % 3 != 0)
+            .Where(number => number % 5 == 0)
+            .ToArbitrary();
+        
+        Prop.ForAll<int>(
+            generatorOfNotSpecialNumbers, (int number) =>
+            {
+                new FizzBuzz().execute(number).Should().Be("Buzz");
+            }).QuickCheckThrowOnFailure();
     }
     
     [Fact]
