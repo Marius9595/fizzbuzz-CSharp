41985a3679c2d68a953cead4d8d40409af412c9b
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-28 18:46:55 +0100

    ✅ Cover all not numbers divisible by three

--- Kata.Tests/FizzBuzzShould.cs
+++ Kata.Tests/FizzBuzzShould.cs
@@ -33,8 +33,17 @@ public class FizzBuzzShould
     [Fact]
     public void turn_into_number_divisible_by_three_to_Fizz_word()
     {
-        int NUMBER_DIVISIBLE_BY_THREE = 3;
-        new FizzBuzz().execute(NUMBER_DIVISIBLE_BY_THREE).Should().Be("Fizz");
+        var generatorOfNotSpecialNumbers = Gen
+            .Choose(0, 100)
+            .Where(number => number % 3 == 0)
+            .Where(number => number % 5 != 0)
+            .ToArbitrary();
+        
+        Prop.ForAll<int>(
+            generatorOfNotSpecialNumbers, (int number) =>
+            {
+                new FizzBuzz().execute(number).Should().Be("Fizz");
+            }).QuickCheckThrowOnFailure();
     }
     
     [Fact]
@@ -48,6 +57,6 @@ public class FizzBuzzShould
     public void turn_into_number_divisible_by_five_and_three_to_FizzBuzz_word()
     {
         int NUMBER_DIVISIBLE_BY_FIVE = 15;
-        new FizzBuzz().execute(NUMBER_DIVISIBLE_BY_FIVE).Should().Be("new FizzBuzz()");
+        new FizzBuzz().execute(NUMBER_DIVISIBLE_BY_FIVE).Should().Be("FizzBuzz");
     }
 }
