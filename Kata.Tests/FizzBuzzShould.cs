14ab9450dbd4854d9eb5257e0c1dce81a2810cd5
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-28 11:48:49 +0100

    🔴 FizzBuzzShould convert_number_divisible_by_three_to_Fizz_word

--- Kata.Tests/FizzBuzzShould.cs
+++ Kata.Tests/FizzBuzzShould.cs
@@ -3,7 +3,7 @@ using FluentAssertions;
 namespace Kata.Tests;
 
 /*
-     1 -> '1'
+     1 -> '1' ✔
      3 -> 'Fizz'
      5 -> 'Buzz'
      15 -> 'FizzBuzz'
@@ -22,5 +22,12 @@ public class FizzBuzzShould
     {
         int NOT_SPECIAL_NUMBER = 1;
         FizzBuzz.execute(NOT_SPECIAL_NUMBER).Should().Be("1");
+    }
+    
+    [Fact]
+    public void turn_into_number_divisible_by_three_to_Fizz_word()
+    {
+        int NUMBER_DIVISIBLE_BY_THREE = 3;
+        FizzBuzz.execute(NUMBER_DIVISIBLE_BY_THREE).Should().Be("Fizz");
     }
 }
